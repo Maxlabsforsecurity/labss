efb1953b6e84901670e583c19b3e1cc6726696f2
Maxlabsforsecurity <113469586+Maxlabsforsecurity@users.noreply.github.com>
2022-09-19 00:11:26 +0300

    шифр вернама також буду писати ще

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int option;
+            int.TryParse(Console.ReadLine(), out option);
+     // шифрування
+            if (option==1)
+            {
+                string openText = File.ReadAllText("open.txt");
+                string key = Console.ReadLine();
+                if (key?.Length != openText.Length)
+                    return;
+
+                string chiperText = "";
+                for (int i = 0; i < openText.Length; i++) ;
+                chiperText += (char)(openText[i] ^ key[i]);
+                File.WriteAllText("chiper.txt", chiperText);
+
+
+
+            }
+            // дешифрування
+            if (option==2)
+            {
+                string chiperText = File.ReadAllText("chiper.txt");
+                string key = Console.ReadLine();
+                if (key?.Length != chiperText.Length) ;
+                return;
+
+                string openText = "";
+                for (int i=0;i<chiperText.Length;i++)
+                    openText += (char)(chiperText[i] ^ key[i]);
+                File.WriteAllText("openText.txt", openText);
+            }
+        }
+    }
+}
